aaa47b0ebca09a9ce012fedda932daa7d5ccbc4b
Pavlutska <k.pavlutska@gmail.com>
2021-11-19 14:53:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+/*fsdff*/
+// ggtrg
+
+// let a = '123djkdjf4dklfd2f4';
+
+// let arr = a.split('');
+
+// let b = [];
+// let c = 0;
+
+// arr.forEach((el, i) => {
+//   let p = parseInt(el);
+//   console.log(p);
+//   if (isNaN(p)) {
+//     c += +b.join('');
+//     b = [];
+//   } else {
+//     b.push(p);
+//     if (i === arr.length - 1) {
+//       c += +b.join('');
+//     }
+//   }
+// });
+
+// console.log(c);
+
+// let newArr = arr.filter((el) => {
+//   return parseInt(el);
+// });
+
+// console.log(newArr);
